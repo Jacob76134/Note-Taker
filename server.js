6820a9a29f6e6294b06c3334903d85a02226742c
Jacob Brewer <jacobbrewerrooney@gmail.com>
2021-11-17 22:52:35 -0500

    heroku port update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(port, () => {
